58d0474c7c95d4b4ceb762f34da201f1c9f718df
luispaivaa <contato.luishenrique2@gmail.com>
2026-06-09 09:20:49 -0300

    docs: Adcionando documentação de estudos

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -1,4 +1,23 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arrays02 {
+    public static void main(String[] args){
+        //byte, short, int, long, float, long e double --> 0
+        // char --> '\u0000' ' '
+        // boolean --> false
+        // String --> null
+
+        String[] nomes = new String[3];
+        nomes[0] = "Goku";
+        nomes[1] = "Gohan";
+        nomes[2] = "Vegeta";
+
+       // nomes = new String[4];
+        //Garbage collector ativado e apaga o ultimo array nomes
+
+
+        for (int i = 0; i < nomes.length ; i++) {
+            System.out.println(nomes[i]);
+        }
+    }
 }
